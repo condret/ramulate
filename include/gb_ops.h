04e796aba3e89aa8685239e3893550b540437e76
condret <condret@runas-racer.com>
2013-12-26 21:58:24 +0100

    add gb_add

--- include/gb_ops.h
+++ include/gb_ops.h
@@ -6,8 +6,13 @@ int gb_ld_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldi_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldd_mov(RReg *reg, const char *dest, const char *src);
 int gb_ld_store_const(RReg *reg, const char *dest, const ut16 src);
+
 int gb_inc(RReg *reg, const char *dest);
 int gb_dec(RReg *reg, const char *dest);
+
+int gb_add(RReg *reg, const char *dest, const char *src);
+
 int gb_swap_reg(RReg *reg, const char *dest);
+
 int gb_jmp_rel(RReg *reg, const st8 dest);
 int gb_call_jmp(RReg* reg, const ut16 dest);
