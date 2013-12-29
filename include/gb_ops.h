3b497382814865dc7f8701202c85740319c343dd
condret <condret@runas-racer.com>
2013-12-29 04:10:12 +0100

    add some ops

--- include/gb_ops.h
+++ include/gb_ops.h
@@ -1,6 +1,5 @@
 /* ramulate - LGPL - Copyright 2013 - condret@runas-racer.com */
 
-#include <r_asm.h>
 #include <r_io.h>
 #include <r_reg.h>
 
@@ -13,12 +12,17 @@ int gb_inc(RReg *reg, const char *dest);
 int gb_dec(RReg *reg, const char *dest);
 
 int gb_add_8(RReg *reg, const char *dest, const char *src);
+int gb_adc_reg(RReg *reg, const char *src);
 
 int gb_sub_reg(RReg *reg, const char *src);
 
 int gb_cp_reg(RReg *reg, const char *src);
 
+int gb_xor(RReg *reg, const ut8 src);
+int gb_xor_reg(RReg *reg, const char *src);
 int gb_swap_reg(RReg *reg, const char *dest);
 
 int gb_jmp_rel(RReg *reg, const st8 dest);
+int gb_jmp_rel_cond(RReg *reg, const char *cond, const st8 dest);
 int gb_call_jmp(RReg* reg, const ut16 dest);
+int gb_call_jmp_cond(RReg *reg, const char *cond, const ut16 dest);
