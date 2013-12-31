b35f23813e8faa2f2d175f1fac2f0a22849bf78f
condret <condret@runas-racer.com>
2013-12-31 10:16:49 +0100

    new toys + better foo

--- include/gb_ops.h
+++ include/gb_ops.h
@@ -3,6 +3,9 @@
 #include <r_io.h>
 #include <r_reg.h>
 
+int gb_push(RIO *io, RReg *reg, const char *src);
+int gb_pop(RIO *io, RReg *reg, const char *src);
+
 int gb_ld_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldi_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldd_mov(RReg *reg, const char *dest, const char *src);
@@ -27,5 +30,8 @@ int gb_jmp_rel_cond(RReg *reg, const char *cond, const st8 dest);
 int gb_jmp(RReg* reg, const ut16 dest);
 int gb_jmp_cond(RReg *reg, const char *cond, const ut16 dest);
 
-int gb_call(RReg *reg, const ut16 dest);
-int gb_call_cond(RReg *reg, const char *cond, const ut16 dest);
+int gb_call(RIO *io, RReg *reg, const ut16 dest);
+int gb_call_cond(RIO *io, RReg *reg, const char *cond, const ut16 dest);
+
+int gb_ret(RIO *io, RReg *ret);
+int gb_ret_cond(RIO *io, RReg *reg, const char *cond);
