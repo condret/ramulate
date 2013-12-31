d32afb8ce26b83bf6e6dd5cba1985176f8578f07
condret <condret@runas-racer.com>
2013-12-31 22:35:57 +0100

    moar ops

--- include/gb_ops.h
+++ include/gb_ops.h
@@ -6,10 +6,15 @@
 int gb_push(RIO *io, RReg *reg, const char *src);
 int gb_pop(RIO *io, RReg *reg, const char *src);
 
+int gb_ei(RIO *io);
+int gb_di(RIO *io);
+
 int gb_ld_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldi_mov(RReg *reg, const char *dest, const char *src);
 int gb_ldd_mov(RReg *reg, const char *dest, const char *src);
 int gb_ld_store_const(RReg *reg, const char *dest, const ut16 src);
+int gb_ld_load_to(RIO *io, RReg *reg, const ut16 dest, const char *src);
+int gb_ld_store_from(RIO *io, RReg *reg, const char *dest, const ut16 src);
 
 int gb_inc(RReg *reg, const char *dest);
 int gb_dec(RReg *reg, const char *dest);
@@ -17,13 +22,16 @@ int gb_dec(RReg *reg, const char *dest);
 int gb_add_8(RReg *reg, const char *dest, const char *src);
 int gb_adc_reg(RReg *reg, const char *src);
 
+int gb_sub_const(RReg *reg, const ut8 src);
 int gb_sub_reg(RReg *reg, const char *src);
 
+int gb_cp_const(RReg *reg, const ut8 src);
 int gb_cp_reg(RReg *reg, const char *src);
 
 int gb_xor(RReg *reg, const ut8 src);
 int gb_xor_reg(RReg *reg, const char *src);
 int gb_swap_reg(RReg *reg, const char *dest);
+int gb_res(RReg *reg, const ut8 arg, const char *dest);
 
 int gb_jmp_rel(RReg *reg, const st8 dest);
 int gb_jmp_rel_cond(RReg *reg, const char *cond, const st8 dest);
@@ -35,3 +43,4 @@ int gb_call_cond(RIO *io, RReg *reg, const char *cond, const ut16 dest);
 
 int gb_ret(RIO *io, RReg *ret);
 int gb_ret_cond(RIO *io, RReg *reg, const char *cond);
+int gb_reti(RIO *io, RReg *reg);
