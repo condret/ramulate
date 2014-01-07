c51f13223bed5dabe950a11b92fb7dbde93cb578
condret <condret@runas-racer.com>
2014-01-08 00:22:31 +0100

    moar ops

--- gb_emu/gb_ops.c
+++ gb_emu/gb_ops.c
@@ -33,22 +33,18 @@ int gb_pop(RIO *io, RReg *reg, const char *dest)					//might be weak
 	return r_reg_set_value(reg, r_reg_get(reg, "sp", -1), r_reg_getv(reg, "sp") +2);
 }
 
-int gb_ei(RIO *io)
+int gb_ei(RReg *reg)
 {
-	if(!io)
+	if(!reg)
 		return R_FALSE;
-	ut8 buf = 1;
-	r_io_cache_write(io, 0xffff, &buf, 1);						//todo: enum for hw-regs
-	return R_TRUE;
+	return r_reg_set_value(reg, r_reg_get(reg, "ime", -1), R_TRUE);
 }
 
-int gb_di(RIO *io)
+int gb_di(RReg *reg)
 {
-	if(!io)
+	if(!reg)
 		return R_FALSE;
-	ut8 buf = 0;
-	r_io_cache_write(io, 0xffff, &buf, 1);						//todo: enum for hw-regs
-	return R_TRUE;
+	return r_reg_set_value(reg, r_reg_get(reg, "ime", -1), R_FALSE);
 }
 
 int gb_ld_mov(RReg *reg, const char *dest, const char *src)
@@ -83,9 +79,17 @@ int gb_ld_load_to(RIO *io, RReg *reg, const ut16 dest, const char *src)
 {
 	if(!(io && reg && src))
 		return R_FALSE;
-	if(dest < 0x8000)
-		return R_TRUE;
 	ut8 buf = r_reg_getv(reg, src);
+	if(dest < 0x8000) {
+		if(dest < 0x4000 && dest > 0x1fff) {
+			if(buf == 0x20)
+				return r_reg_set_value(reg, r_reg_get(reg, "mbc", -1), 0);
+			if(buf)
+				return r_reg_set_value(reg, r_reg_get(reg, "mbc", -1), buf - 1);
+			return r_reg_set_value(reg, r_reg_get(reg, "mbc", -1), buf);
+		}
+		return R_TRUE;
+	}
 	r_io_cache_write(io, dest, &buf, 1);
 	return R_TRUE;
 }
@@ -113,19 +117,31 @@ int gb_dec(RReg *reg, const char *dest)							//dec
 	return r_reg_set_value(reg, r_reg_get(reg, dest, -1), r_reg_getv(reg, dest)-1);
 }
 
-int gb_add_8(RReg *reg, const char *dest, const char *src)
+int gb_add_8(RReg *reg, const char *src)
 {
-	if(!(reg && dest && src))
+	if(!(reg && src))
 		return R_FALSE;
-	int ret = r_reg_set_value(reg, r_reg_get(reg, dest, -1), (ut8)(r_reg_getv(reg, dest) + r_reg_getv(reg, src)));
-	if(r_reg_getv(reg, dest))
+	int ret = r_reg_set_value(reg, r_reg_get(reg, "a", -1), (r_reg_getv(reg, "a") + r_reg_getv(reg, src)));
+	if(r_reg_getv(reg, "a"))
 		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
 	else
 		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
 	return ret;
 }
 
-int gb_adc_reg(RReg *reg, const char *src)
+int gb_add_8_at(RIO *io, RReg *reg, const ut16 src)
+{
+	ut8 dval;
+	r_io_cache_read(io, src, &dval, 1);
+	int ret = r_reg_set_value(reg, r_reg_get(reg, "a", -1), r_reg_getv(reg, "a") + dval);
+	if( r_reg_getv(reg, "a"))
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
+	return ret;
+}
+
+int gb_adc(RReg *reg, const char *src)
 {
 	if(!(reg && src))
 		return R_FALSE;
@@ -148,6 +164,10 @@ int gb_sub_const(RReg *reg, const ut8 src)
 	if(!reg)
 		return R_FALSE;
 	ut8 dval = r_reg_getv(reg, "a") - src;
+	if(r_reg_getv(reg, "a") < src)
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
 	if(dval)
 		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
 	else
@@ -156,11 +176,18 @@ int gb_sub_const(RReg *reg, const ut8 src)
 	return r_reg_set_value(reg, r_reg_get(reg, "a", -1), dval);
 }
 
-int gb_sub_reg(RReg *reg, const char *src)
+int gb_sub(RReg *reg, const char *src)
 {
 	if(!(reg && src))
 		return R_FALSE;
 	return gb_sub_const(reg, r_reg_getv(reg, src));
+}
+
+int gb_sbc(RReg *reg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	return gb_sub_const(reg, r_reg_getv(reg, src) - r_reg_getv(reg, "C"));
 }
 
 int gb_cp_const(RReg *reg, const ut8 src)
@@ -173,14 +200,41 @@ int gb_cp_const(RReg *reg, const ut8 src)
 	return ret;
 }
 
-int gb_cp_reg(RReg *reg, const char *src)
+int gb_cp(RReg *reg, const char *src)
 {
 	if(!src)
 		return R_FALSE;
 	return gb_cp_const(reg, r_reg_getv(reg, src));
 }
 
-int gb_xor(RReg *reg, const ut8 src){
+int gb_and(RReg *reg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	ut8 dval = r_reg_getv(reg, "a") & r_reg_getv(reg, src);
+	if (dval) {
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
+	} else {
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
+	}
+	return r_reg_set_value(reg, r_reg_get(reg, "a", -1), dval);
+}
+
+int gb_and_at(RIO *io, RReg *reg, const ut16 src)
+{
+	if(!(io && reg))
+		return R_FALSE;
+	ut8 dval;
+	r_io_cache_read(io, src, &dval, 1);
+	dval = r_reg_getv(reg, "a") & dval;
+	if(dval)
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
+	return r_reg_set_value(reg, r_reg_get(reg, "a", -1), dval);
+}
+
+int gb_xor_const(RReg *reg, const ut8 src){
 	if(!reg)
 		return R_FALSE;
 	r_reg_set_value(reg, r_reg_get(reg, "N", -1), R_FALSE);
@@ -193,14 +247,14 @@ int gb_xor(RReg *reg, const ut8 src){
 	return r_reg_set_value(reg, r_reg_get(reg, "a", -1), r_reg_getv(reg, "a") ^ src);
 }
 
-int gb_xor_reg(RReg *reg, const char *src)
+int gb_xor(RReg *reg, const char *src)
 {
 	if(!(reg && src))
 		return R_FALSE;
-	return gb_xor(reg, r_reg_getv(reg, src));
+	return gb_xor_const(reg, r_reg_getv(reg, src));
 }
 
-int gb_swap_reg(RReg *reg, const char *dest)
+int gb_swap(RReg *reg, const char *dest)
 {
 	if(!(reg && dest))
 		return R_FALSE;
@@ -208,7 +262,16 @@ int gb_swap_reg(RReg *reg, const char *dest)
 	return r_reg_set_value(reg, r_reg_get(reg, dest, -1), (swap>>4) + (swap<<4));
 }
 
-//todo: int gb_swap_ptr(RIO *io, RReg *reg, const ut16 dest)
+int gb_swap_at(RIO *io, const ut16 dest)
+{
+	if((!io) && dest < 0x8000)
+		return R_FALSE;
+	ut8 swap;
+	r_io_cache_read(io, dest, &swap, 1);
+	swap = ((swap>>4) + (swap<<4));
+	r_io_cache_write(io, dest, &swap, 1);
+	return R_TRUE;
+}
 
 int gb_res(RReg *reg, const ut8 arg, const char *dest)
 {
@@ -219,15 +282,157 @@ int gb_res(RReg *reg, const ut8 arg, const char *dest)
 	return r_reg_set_value(reg, r_reg_get(reg, dest, -1), dval);
 }
 
+int gb_res_at(RIO *io, const ut8 arg, const ut16 dest)
+{
+	if((!io) || dest < 0x8000)
+		return R_FALSE;
+	ut8 res = 0x1<<arg;
+	ut8 dval;
+	r_io_cache_read(io, dest, &dval, 1);
+	dval = (dval & (~res));
+	r_io_cache_write(io, dest, &dval, 1);
+	return R_TRUE;
+}
+
+int gb_set(RReg *reg, const ut8 arg, const char *dest)
+{
+	if(!(reg && dest))
+		return R_FALSE;
+	ut8 set = 0x1<<arg;
+	ut8 dval = (r_reg_getv(reg, dest) | set);
+	return r_reg_set_value(reg, r_reg_get(reg, dest, -1), dval);
+}
+
+int gb_set_at(RIO *io, const ut8 arg, const ut16 dest)
+{
+	if((!io) || dest < 0x8000)
+		return R_FALSE;
+	ut8 set = 0x1<<arg;
+	ut8 dval;
+	r_io_cache_read(io, dest, &dval, 1);
+	dval = (dval | set);
+	r_io_cache_write(io, dest, &dval, 1);
+	return R_TRUE;
+}
+
+int gb_bit(RReg *reg, const ut8 arg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	ut8 bit = 0x1<<arg;
+	if (r_reg_getv(reg, src) & bit)
+		return r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
+	return r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
+}
+
+int gb_bit_at(RIO *io, RReg *reg, const ut8 arg, const ut16 src)
+{
+	if((!(io && reg)) || src < 0x8000)
+		return R_FALSE;
+	ut8 bit = 0x1<<arg;
+	ut8 sval;
+	r_io_cache_read(io, src, &sval, 1);
+	if(sval & bit)
+		return r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_TRUE);
+	return r_reg_set_value(reg, r_reg_get(reg, "Z", -1), R_FALSE);
+}
+
+int gb_sla(RReg *reg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	ut8 dval = r_reg_getv(reg, src);
+	if(dval & (0x1<<7))
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
+	return r_reg_set_value(reg, r_reg_get(reg, src, -1), dval<<1);
+}
+
+int gb_sla_at(RIO *io, RReg *reg, const ut16 src)
+{
+	if(!(io && reg) || src < 0x8000)
+		return R_FALSE;
+	ut8 dval;
+	r_io_cache_read(io, src, &dval, 1);
+	if(dval & (0x1<<7))
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
+	dval = dval<<1;
+	r_io_cache_write(io, src, &dval, 1);
+	return R_TRUE;
+}
+
+int gb_srl(RReg *reg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	ut8 dval = r_reg_getv(reg, src);
+	if (dval & 0x1)
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
+	return r_reg_set_value(reg, r_reg_get(reg, src, -1), dval>>1);
+}
+
+int gb_srl_at(RIO *io, RReg *reg, const ut16 src)
+{
+	if(!(io && reg) || src < 0x8000)
+		return R_FALSE;
+	ut8 dval;
+	r_io_cache_read(io, src, &dval, 1);
+	if (dval & 0x1)
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
+	r_io_cache_write(io, src, &dval, 1);
+	return R_TRUE;
+}
+
+int gb_sra(RReg *reg, const char *src)
+{
+	if(!gb_srl(reg, src))
+		return R_FALSE;
+	return r_reg_set_value(reg, r_reg_get(reg, src, -1), (r_reg_getv(reg, src) | (r_reg_getv(reg, src) & (0x1<<6))<<7));
+}
+
+int gb_sra_at(RIO *io, RReg *reg, const ut16 src)
+{
+	if(!gb_srl_at(io, reg, src))
+		return R_FALSE;
+	ut8 dval;
+	r_io_cache_read(io, src, &dval, 1);
+	dval = (dval | ((dval & (0x1<<6))<<7));
+	r_io_cache_write(io, src, &dval, 1);
+	return R_TRUE;
+}
+
+int gb_rr(RReg *reg, const char *src)
+{
+	if(!(reg && src))
+		return R_FALSE;
+	ut8 dval = r_reg_getv(reg, src);
+	if(r_reg_getv(reg, "C"))
+		gb_set(reg, 7, "a");
+	else
+		gb_res(reg, 7, "a");
+	dval = (dval>>1) + (dval<<7);
+	if(dval & 0x1)
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_TRUE);
+	else
+		r_reg_set_value(reg, r_reg_get(reg, "C", -1), R_FALSE);
+	return r_reg_set_value(reg, r_reg_get(reg, src, -1), dval);
+}
+
 int gb_jmp(RReg *reg, const ut16 dest)
 {
 	if(!reg)
 		return R_FALSE;
-	if(dest>0x3fff && dest<0x8000) {						//bankswitches
+	if(dest>0x3fff && dest<0x8000)						//bankswitches
 		r_reg_set_value(reg, r_reg_get(reg, "m", -1), r_reg_getv(reg, "mbc"));
-	} else {
+	else
 		r_reg_set_value(reg, r_reg_get(reg, "m", -1), 0);
-	}
 	return r_reg_set_value(reg, r_reg_get(reg, "pc", -1), dest);
 }
 
@@ -310,5 +515,5 @@ int gb_ret_cond(RIO *io, RReg *reg, const char *cond)
 
 int gb_reti(RIO *io, RReg *reg)
 {
-	return (gb_ei(io) && gb_ret(io, reg));
+	return (gb_ei(reg) && gb_ret(io, reg));
 }
