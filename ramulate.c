d680aaf5ff04d4c1a32cdc64b76d83b4db5acf3c
condret <condret@runas-racer.com>
2014-01-10 17:38:25 +0100

    emu_io

--- ramulate.c
+++ ramulate.c
@@ -5,6 +5,7 @@
 #include <r_bin.h>
 #include <r_asm.h>
 #include <gb_emu.h>
+#include <stdlib.h>
 
 void gb_init(GBemu *gb);
 
@@ -33,10 +34,6 @@ void main(int argc, char *argv[])
 	}
 	ut32 counter=0;
 	gb_init(gb);
-	show_regs(gb->reg,32);
-	show_regs(gb->reg,16);
-	show_regs(gb->reg,8);
-	show_regs(gb->reg,1);
 	while(gb_step(gb) && counter!=0xff)
 		counter++;
 	eprintf("\n%i ops were emulated!!!\n",counter);
@@ -51,48 +48,51 @@ void gb_init(GBemu *gb)
 	r_asm_setup(gb->a, "gb", 8, 0);
 	gb->mbc->type = gb_get_mbc(gb->io);
 	gb_sections(gb->io, gb->bin);
+	gb->c->len = 37268;
+	gb->c->base = 0x8000;
+	gb->c->buf = malloc(gb->c->len);
 	ut8 wbuf=0;
-	r_io_cache_write(gb->io, 0xff05, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff06, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff07, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff17, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff21, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff22, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff42, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff43, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff45, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff4a, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff4b, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xffff, &wbuf, 1);
+	emu_write(gb, 0xff05, &wbuf, 1);
+	emu_write(gb, 0xff06, &wbuf, 1);
+	emu_write(gb, 0xff07, &wbuf, 1);
+	emu_write(gb, 0xff17, &wbuf, 1);
+	emu_write(gb, 0xff21, &wbuf, 1);
+	emu_write(gb, 0xff22, &wbuf, 1);
+	emu_write(gb, 0xff42, &wbuf, 1);
+	emu_write(gb, 0xff43, &wbuf, 1);
+	emu_write(gb, 0xff45, &wbuf, 1);
+	emu_write(gb, 0xff4a, &wbuf, 1);
+	emu_write(gb, 0xff4b, &wbuf, 1);
+	emu_write(gb, 0xffff, &wbuf, 1);
 	wbuf = 0x80;
-	r_io_cache_write(gb->io, 0xff10, &wbuf, 1);
+	emu_write(gb, 0xff10, &wbuf, 1);
 	wbuf = 0xbf;
-	r_io_cache_write(gb->io, 0xff11, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff14, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff19, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff1e, &wbuf, 1);
+	emu_write(gb, 0xff11, &wbuf, 1);
+	emu_write(gb, 0xff14, &wbuf, 1);
+	emu_write(gb, 0xff19, &wbuf, 1);
+	emu_write(gb, 0xff1e, &wbuf, 1);
 	wbuf = 0xf3;
-	r_io_cache_write(gb->io, 0xff12, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff25, &wbuf, 1);
+	emu_write(gb, 0xff12, &wbuf, 1);
+	emu_write(gb, 0xff25, &wbuf, 1);
 	wbuf = 0x3f;
-	r_io_cache_write(gb->io, 0xff16, &wbuf, 1);
+	emu_write(gb, 0xff16, &wbuf, 1);
 	wbuf = 0x7f;
-	r_io_cache_write(gb->io, 0xff1a, &wbuf, 1);
+	emu_write(gb, 0xff1a, &wbuf, 1);
 	wbuf = 0xff;
-	r_io_cache_write(gb->io, 0xff1b, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff20, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff48, &wbuf, 1);
-	r_io_cache_write(gb->io, 0xff49, &wbuf, 1);
+	emu_write(gb, 0xff1b, &wbuf, 1);
+	emu_write(gb, 0xff20, &wbuf, 1);
+	emu_write(gb, 0xff48, &wbuf, 1);
+	emu_write(gb, 0xff49, &wbuf, 1);
 	wbuf = 0x9f;
-	r_io_cache_write(gb->io, 0xff1c, &wbuf, 1);
+	emu_write(gb, 0xff1c, &wbuf, 1);
 	wbuf = 0x77;
-	r_io_cache_write(gb->io, 0xff24, &wbuf, 1);
+	emu_write(gb, 0xff24, &wbuf, 1);
 	wbuf = 0xf3;
-	r_io_cache_write(gb->io, 0xff25, &wbuf, 1);
+	emu_write(gb, 0xff25, &wbuf, 1);
 	wbuf = 0xf1;
-	r_io_cache_write(gb->io, 0xff26, &wbuf, 1);
+	emu_write(gb, 0xff26, &wbuf, 1);
 	wbuf = 0x91;
-	r_io_cache_write(gb->io, 0xff40, &wbuf, 1);
+	emu_write(gb, 0xff40, &wbuf, 1);
 	wbuf = 0xfc;
-	r_io_cache_write(gb->io, 0xff47, &wbuf, 1);
+	emu_write(gb, 0xff47, &wbuf, 1);
 }
