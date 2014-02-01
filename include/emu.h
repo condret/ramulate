7e4db87bcf8c9de454bd10795bae815b15b2f017
condret <condret@runas-racer.com>
2014-02-01 22:41:02 +0100

    Update emu.h

--- include/emu.h
+++ include/emu.h
@@ -23,6 +23,7 @@ typedef struct emu_t {
 //	RList *inverse;		<-- this will enable backwards-execution
 	RAsm *a;
 	RAsmOp *op;
+	void *data;		//<-- storage for arch-specific info, such as mbc-type : must be freed manually
 } emu;
 
 
@@ -40,14 +41,16 @@ emu *emu_new();
 void emu_free(emu *e);
 /* --- emu/emu.c --- */
 
-
+/* --- vsection --- */
+#define	VS_TO_END(addr,vs)	((vs->addr) + (vs->size) - (addr))
+#define VS_TO_BEGIN(addr,vs)	((addr) - (vs->addr))
 /* --- emu/vsection.c --- */
 int virtual_section_add(emu *e, ut64 addr, ut64 size, int rwx, const char *name);
 VSection *virtual_section_get_addr(emu *e, ut64 addr);
 VSection *virtual_section_get_name(emu *e, char *name);
 VSection *virtual_section_get_i(emu *e, int idx);
-//VSection *virtual_section_get_next_to_addr(emu *e, ut64 addr);
-//VSection *virtual_section_get_prev_to_addr(emu *e, ut64 addr);
+VSection *virtual_section_get_next_to_addr(emu *e, ut64 addr);
+VSection *virtual_section_get_prev_to_addr(emu *e, ut64 addr);
 int virtual_section_rm_i(emu *e, int idx);
 int virtual_section_read(VSection *vs, ut64 addr, ut8 *buf, ut64 len);
 int virtual_section_write(VSection *vs, ut64 addr, ut8 *buf, ut64 len);
@@ -62,6 +65,6 @@ VSection *virtual_section_resolve_link(emu *e, VSection *vs_src);
 
 
 /* --- emu/e_io.c --- */
-//int emu_read(emu *e, ut64 addr, ut8 *buf, ut64 len);
-//int emu_write(emu *e, ut64 addr, ut8 *buf, ut64 len);
+int emu_read(emu *e, ut64 addr, ut8 *buf, ut64 len);
+int emu_write(emu *e, ut64 addr, ut8 *buf, ut64 len);
 /* --- emu/e_io.c --- */
