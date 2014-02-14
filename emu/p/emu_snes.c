770ae51fc349a9fe4865ae21614585f3d5f513d9
condret <condret@runas-racer.com>
2014-02-14 12:53:35 +0100

    plugin-system, clean-up ...

---
+++ emu/p/emu_snes.c
@@ -0,0 +1,22 @@
+#include <emu.h>
+#include <r_lib.h>
+
+EPlugin snes_emu = {
+	.arch = "snes",
+	.desc = "Snes emulation plugin - TODO",
+	.license = "LGPL3",
+	.deps = (EMU_PLUGIN_DEP_ASM | EMU_PLUGIN_DEP_ANAL),
+	.min_read_sz = 10,
+	.step = NULL,
+	.set_vs_profile = NULL,
+	.set_reg_profile = NULL,
+	.read = NULL,
+	.handle_user_input = NULL,
+	.allocate_data = NULL,
+	.free_data = NULL,
+};
+
+struct r_lib_struct_t ramulate_plugin = {
+	.type = RAMULATE_EMU_PLUGIN,
+	.data = &snes_emu
+};
