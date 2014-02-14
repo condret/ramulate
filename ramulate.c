45fb1103a005fbcdd9e6dc6bf78ec3f693bd49d6
condret <condret@runas-racer.com>
2014-02-14 12:41:14 +0100

    plugin-system, clean-up ...

--- ramulate.c
+++ ramulate.c
@@ -1,70 +1,147 @@
-/* ramulate - LGPL - Copyright 2013 - condret@runas-racer.com */
+/* ramulate - LGPL - Copyright 2013 - 2014 - condret@runas-racer.com */
 
 #include <r_reg.h>
 #include <r_io.h>
 #include <r_bin.h>
+#include <r_lib.h>
 #include <r_asm.h>
 #include <r_util.h>
+#include <r_anal.h>
 #include <emu.h>
-#include <gb.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_vs(emu *e)
-{
-	VSection *vs;
-	RListIter *iter;
-	r_list_foreach(e->vsections, iter, vs) {
-		eprintf("id: [%i]\t\tname: vsection.%s\t\taddr: 0x%"PFMT64x"\t\tsize:0x%"PFMT64x"\tPerms: %s\t",
-			vs->id, vs->name, vs->addr, vs->size, r_str_rwx_i(vs->rwx));
-		switch(vs->linked) {
-			case VS_NO_LINK:
-				eprintf("not linked\n");
-				break;
-			case VS_IS_LINKED:
-				eprintf("is linked to vsection.%s\n", virtual_section_resolve_link(e, vs)->name);
-				break;
-			case VS_HAS_LINKED:
-				eprintf("has linked\n");
-				break;
-		}
-	}
-}
+inline void set_sections (RIO *io, RBin *bin);
+static int emulation (emu *e, ut8 *buf);
 
 int main(int argc, char *argv[])
 {
-	if(argc<2) {
-		eprintf("which rom?\n");
+	emu *e;
+	ut8 *buf;
+	if (argc<2) {
+		eprintf ("Which rom?\n");
 		return R_FALSE;
 	}
-	emu *e = emu_new();
-	e->io->fd = r_io_open(e->io, argv[1], R_IO_READ, 0);
-	if(!e->io->fd) {
-		eprintf("Wrong path?\n");
+	e = emu_new();
+	e->io->fd = r_io_open (e->io, argv[1], R_IO_READ, 0);
+	if (!e->io->fd) {
+		eprintf ("Wrong Path\n");
 		emu_free(e);
 		return R_FALSE;
 	}
-	RBin *bin = r_bin_new();
-	r_bin_load(bin, argv[1], 0);
-	if(!(	bin->cur.o->info->arch[0]=='g' &&				//pastafari
-		bin->cur.o->info->arch[1]=='b' &&
-		bin->cur.o->info->arch[2]==0)) {
-			eprintf("this is not a gb-rom\n");
-			r_io_close(e->io, e->io->fd);
-			r_bin_free(bin);
-			emu_free(e);
-			return R_FALSE;
+
+	r_bin_load (e->bin, argv[1], 0, 0, 0);
+	if (!e->bin->cur->o->info) {
+		eprintf("No such bin plugin\n");
+		r_io_close (e->io, e->io->fd);
+		emu_free(e);
+		return R_FALSE;
 	}
-	r_asm_setup(e->a, "gb", 8, 0);
-	gb_sections(e->io, bin);
-	gb_reg_profile(e, bin);
-	r_bin_free(bin);
-	e->data = gb_mbc_new();
-	gb_get_mbc(e->io, e->data);
-	gb_vsections(e);
-	while(r_reg_getv(e->reg, "mpc")<0x10000)
-		gb_step(e);
-	gb_mbc_free(e->data);
-	r_io_close(e->io,e->io->fd);
-	emu_free(e);
+
+	r_lib_opendir (e->lib, "/lib/ramulate");
+	emu_use (e, e->bin->cur->o->info->arch);
+
+	if(!e->plugin) {
+		eprintf ("No such emu plugin for %s/%s\nBut we have these:\n",
+				e->bin->cur->o->info->arch,
+				e->bin->cur->o->info->machine);
+		emu_list_plugins (e);
+		emu_free(e);
+		return R_FALSE;
+	}
+
+	printf ("EMU:\t%s\t%s\n",e->plugin->arch, e->plugin->desc);
+
+	if (r_asm_use (e->a, e->plugin->arch)) {
+		printf("R_ASM:\t%s\n", e->plugin->arch);
+	} else {
+		printf("R_ASM:\tNONE\n");
+		if (e->plugin->deps & EMU_PLUGIN_DEP_ASM) {					//check asm-plugin-dependencies
+			eprintf("Missing asm-plugin-dependency, please update your r2-version\n");
+			r_io_close (e->io, e->io->fd);
+			emu_free (e);
+			return R_FALSE;
+		}
+	}
+
+
+	if (r_anal_use (e->anal, e->plugin->arch)) {						//check anal-plugin-dependecies
+		printf("R_ANAL:\t%s\n", e->plugin->arch);
+	} else {
+		printf("R_ANAL:\tNONE\n");
+		if (e->plugin->deps & EMU_PLUGIN_DEP_ANAL) {
+			eprintf("Missing anal-plugin-dependency, please update your r2-version\n");
+			r_io_close (e->io, e->io->fd);
+			emu_free (e);
+		}
+	}
+
+	if (e->plugin->allocate_data && e->plugin->free_data)
+		e->plugin->allocate_data (e);
+
+	set_sections (e->io, e->bin);
+
+	if (e->plugin->set_reg_profile)
+		e->plugin->set_reg_profile (e);
+
+	if (e->plugin->set_vs_profile)
+		e->plugin->set_vs_profile (e);
+
+	if (e->plugin->min_read_sz)
+		buf = malloc (e->plugin->min_read_sz);
+	else	buf = malloc (sizeof(int));							//LOOOOOOOOOL :D :D :D
+
+	if (e->plugin->step)
+		emulation (e, buf);
+	else printf ("cannot emulate, please check that plugin\n");
+
+	free (buf);
+
+	if (e->plugin->allocate_data && e->plugin->free_data)
+		e->plugin->free_data (e->data);
+	r_io_close (e->io, e->io->fd);
+	emu_free (e);
 	return R_TRUE;
+}
+
+
+inline void set_sections(RIO *io, RBin *bin)
+{
+	RBinSection *section;
+	RListIter *iter;
+	RList *sections = r_bin_get_sections (bin);
+	r_list_foreach(sections, iter, section) {
+		r_io_section_add (io,	section->offset, section->rva, section->size,
+					section->vsize, section->srwx, section->name);
+	}
+}
+
+
+static int emulation (emu *e, ut8 *buf)
+{
+	ut64 addr = r_reg_getv (e->reg, r_reg_get_name (e->reg, R_REG_NAME_PC));		//Check Breakboints here: new return stat for that
+	if (e->plugin->read) {
+		if (e->plugin->min_read_sz)
+			e->plugin->read (e, addr, buf, e->plugin->min_read_sz);
+		else	e->plugin->read (e, addr, buf, sizeof(int));
+	} else {
+		if (e->plugin->min_read_sz)
+			emu_read (e, addr, buf, e->plugin->min_read_sz);
+		else	emu_read (e, addr, buf, sizeof(int));
+	}
+
+	if (e->plugin->deps & EMU_PLUGIN_DEP_ASM) {						//only disassemble if it is necessary
+		r_asm_set_pc (e->a, addr);
+		if (e->plugin->min_read_sz)
+			r_asm_disassemble (e->a, e->op, buf, e->plugin->min_read_sz);
+		else	r_asm_disassemble (e->a, e->op, buf, sizeof(int));
+	}
+
+	if (e->plugin->deps & EMU_PLUGIN_DEP_ANAL) {						//only analize if it is necessary
+		if (e->plugin->min_read_sz)
+			r_anal_op (e->anal, e->anop, addr, buf, e->plugin->min_read_sz);
+		else	r_anal_op (e->anal, e->anop, addr, buf, sizeof(int));
+	}
+
+	return e->plugin->step (e, buf);
 }
