3b5549987cc48c6648fb8d6a43844935838d9853
condret <condret@runas-racer.com>
2014-03-28 14:03:52 +0100

    remove ugly parser

--- emu/arch/include/gb.h
+++ emu/arch/include/gb.h
@@ -52,78 +52,13 @@ int gb_step(emu *e, ut8 *buf);
 int gb_read(emu *e, ut64 addr, ut8 *buf, ut32 len);
 int gb_write(emu *e, ut64 addr, ut8 *buf, ut32 len);
 
-
-// ops
-int gb_push(emu *e, RReg *reg, const char *src);
-int gb_pop(emu *e, RReg *reg, const char *src);
-
-int gb_ei(RReg *reg);
-int gb_di(RReg *reg);
-
-int gb_ld_mov(RReg *reg, const char *dest, const char *src);
-int gb_ld_store_const(RReg *reg, const char *dest, const ut16 src);
-int gb_ld_load_to(emu *e, RReg *reg, const ut16 dest, const char *src);
-int gb_ld_load_const_to(emu *e, const ut16 dest, ut8 src);
-int gb_ld_store_from(emu *e, RReg *reg, const char *dest, ut16 src);
-
-int gb_inc(RReg *reg, const char *dest);
-int gb_inc_at(emu *e, RReg *reg, const ut16 src);
-int gb_dec(RReg *reg, const char *dest);
-
-int gb_add_8_const(RReg *reg, const ut8 src);
-int gb_add_8(RReg *reg, const char *src);
-int gb_add_8_at(emu *e, RReg *reg, const ut16 src);
-int gb_add_16(RReg *reg, const char *dest, const char *src);
-int gb_adc(RReg *reg, const char *src);
-
-int gb_sub_const(RReg *reg, const ut8 src);
-int gb_sub(RReg *reg, const char *src);
-int gb_sbc(RReg *reg, const char *src);
-
-int gb_cp_const(RReg *reg, const ut8 src);
-int gb_cp(RReg *reg, const char *src);
-int gb_cp_at(emu *e, RReg *reg, const ut16 src);
-
-int gb_and_const(RReg *reg, ut8 src);
-int gb_and(RReg *reg, const char *src);
-int gb_and_at(emu *e, RReg *reg, const ut16 src);
-int gb_or_const(RReg *reg, const ut8 src);
-int gb_or(RReg *reg, const char *src);
-int gb_or_at(emu *e, RReg *reg, const ut16 src);
-int gb_xor_const(RReg *reg, const ut8 src);
-int gb_xor(RReg *reg, const char *src);
-int gb_xor_at(emu *e, RReg *reg, const ut16 src);
-int gb_swap(RReg *reg, const char *dest);
-int gb_swap_at(emu *e, const ut16 dest);
-int gb_res(RReg *reg, const ut8 arg, const char *dest);
-int gb_res_at(emu *e, const ut8 arg, const ut16 dest);
-int gb_set(RReg *reg, const ut8 arg, const char *dest);
-int gb_set_at(emu *e, const ut8 arg, const ut16 dest);
-int gb_bit(RReg *reg, const ut8 arg, const char *src);
-int gb_bit_at(emu *e, RReg *reg, const ut8 arg, const ut16);
-int gb_sla(RReg *reg, const char *src);
-int gb_sla_at(emu *e, RReg *reg, const ut16 src);
-int gb_srl(RReg *reg, const char *src);
-int gb_srl_at(emu *e, RReg *reg, const ut16 src);
-int gb_sra(RReg *reg, const char *src);
-int gb_sra_at(emu *e, RReg *reg, const ut16 src);
-int gb_rr(RReg *reg, const char *src);
-int gb_rr_at(emu *e, RReg *reg, const ut16 src);
-int gb_rl(RReg *reg, const char *src);
-int gb_rl_at(emu *e, RReg *reg, const ut16 src);
-int gb_rrc(RReg *reg, const char *src);
-int gb_rlc(RReg *reg, const char *src);
-
-int gb_jmp_rel(RReg *reg, const st8 dest);
-int gb_jmp_rel_cond(RReg *reg, const char *cond, const st8 dest);
-int gb_jmp(RReg* reg, const ut16 dest);
-int gb_jmp_cond(RReg *reg, const char *cond, const ut16 dest);
-
-int gb_call(emu *e, RReg *reg, const ut16 dest);
-int gb_call_cond(emu *e, RReg *reg, const char *cond, const ut16 dest);
-
-int gb_ret(emu *e, RReg *ret);
-int gb_ret_cond(emu *e, RReg *reg, const char *cond);
-int gb_reti(emu *e, RReg *reg);
+//ops
+int gb_mov (emu *e);
+int gb_xor (emu *e);
+int gb_add (emu *e);
+int gb_cp (emu *e);
+int gb_sub (emu *e);
+int gb_jmp (emu *e);
+int gb_cjmp (emu *e);
 
 #endif
