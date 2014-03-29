d568fc5c4aca8024beb49340d743a9d0f17a753f
condret <condret@runas-racer.com>
2014-03-29 23:24:55 +0100

    fix some opcodes

--- emu/arch/gb/gb_ops.c
+++ emu/arch/gb/gb_ops.c
@@ -2,6 +2,7 @@
 
 #include <emu.h>
 #include <gb.h>
+#include <stdlib.h>
 #include <r_asm.h>
 #include <r_io.h>
 #include <r_reg.h>
@@ -57,8 +58,13 @@ int gb_xor (emu *e)
 
 static inline int gb_add_8 (emu *e)
 {
-	ut64 val = r_reg_get_value (e->reg, e->anop->dst->reg), src;
+	ut64 val = r_reg_get_value (e->reg, e->anop->dst->reg), src, addr;
 	ut8 buf[1];
+	if (e->anop->dst->memref) {			//inc [hl]
+		addr = val;
+		gb_read (e, val, buf, 1);
+		val = (ut64)*buf;
+	}
 	if (e->anop->src[0]->reg) {
 		src = r_reg_get_value (e->reg, e->anop->src[0]->reg);
 		if (e->anop->src[0]->memref) {
@@ -68,21 +74,23 @@ static inline int gb_add_8 (emu *e)
 	} else	src = e->anop->src[0]->imm;
 	if (e->anop->src[1])
 		src += r_reg_get_value (e->reg, e->anop->src[1]->reg);
-	r_reg_set_value (e->reg, r_reg_get (e->reg, "C", -1), (((val + src) & 0xff) < val));
+	if (e->op->buf_asm[0] != 'i');
+		r_reg_set_value (e->reg, r_reg_get (e->reg, "C", -1), (((val + src) & 0xff) < val));
 	r_reg_set_value (e->reg, r_reg_get (e->reg, "H", -1), (((val + src) & 0xf) < val));
 	r_reg_set_value (e->reg, r_reg_get (e->reg, "Z", -1), (((val * src) & 0xff) == 0));
 	val += src;
 	val &= 0xff;
+	if (e->anop->dst->memref) {
+		*buf = (ut8)val;
+		return gb_write (e, addr, buf, 1);
+	}
 	return r_reg_set_value (e->reg, e->anop->dst->reg, val);
 }
 
 int gb_add (emu *e)
 {
-	ut64 val = r_reg_get_value (e->reg, e->anop->dst->reg);
-	if (e->op->buf_asm[0] == 'i')
-		r_reg_set_value (e->reg, e->anop->dst->reg, (val+1) &0xffff);	//XXX memrefs
 	r_reg_set_value (e->reg, r_reg_get (e->reg, "N", -1), R_FALSE);
-	if (e->anop->dst->reg->size == 8)
+	if (e->anop->dst->reg->size == 8 || e->anop->dst->memref)
 		return gb_add_8 (e);
 	eprintf ("TODO\n");
 	return R_FALSE;
@@ -104,8 +112,9 @@ int gb_sub (emu *e)
 	} else	src = e->anop->src[0]->imm;
 	if (e->anop->src[1])
 		src += r_reg_get_value (e->reg, e->anop->src[1]->reg);		//sbc
-	if (e->op->buf_asm[0] != 'd') {
-		r_reg_set_value (e->reg, r_reg_get (e->reg, "C", -1), (val<src));
+	if (e->anop->dst->reg->size != 2) {
+		if (e->op->buf_asm[0] != 'd')
+			r_reg_set_value (e->reg, r_reg_get (e->reg, "C", -1), (val<src));
 		r_reg_set_value (e->reg, r_reg_get (e->reg, "H", -1), ((val & 7)<(src & 7)));
 		r_reg_set_value (e->reg, r_reg_get (e->reg, "Z", -1), (val == src));
 	}
@@ -142,4 +151,100 @@ int gb_cjmp (emu *e)
 	if (c!=e->anop->src[0]->imm)
 		return gb_jmp (e);
 	return R_TRUE;
+}
+
+int gb_ujmp (emu *e)
+{
+	if (e->anop->dst)
+		e->anop->jump = r_reg_get_value (e->reg, e->anop->dst->reg);
+	return gb_jmp (e);
+}
+
+int gb_call (emu *e)
+{
+	ut16 pc = (ut16)r_reg_getv (e->reg, "pc");
+	ut16 sp = (ut16)r_reg_getv (e->reg, "sp");
+	sp -= 2;
+	r_reg_setv (e->reg, "sp", (ut64)sp);
+	gb_write (e, (ut64)sp, (ut8*)(&pc), 2);
+	return gb_jmp (e);
+}
+
+int gb_ccall (emu *e)
+{
+	ut8 c = r_reg_get_value (e->reg, e->anop->dst->reg);
+	if (e->anop->cond == R_ANAL_COND_EQ) {
+		if (c==e->anop->src[0]->imm)
+			return gb_call (e);
+		return R_TRUE;
+	}
+	if (c!=e->anop->src[0]->imm)
+		return gb_call (e);
+	return R_TRUE;
+}
+
+int gb_ret (emu *e)
+{
+	ut16 sp = (ut16)r_reg_getv (e->reg, "sp");
+	ut16 pc = 0;
+	ut32 mpc = 0;
+	gb_read (e, (ut64)sp, (ut8*)(&pc), 2);
+	mpc = pc;
+	if (0x3fff < pc && pc < 0x8000)
+		mpc += 0x10000 * r_reg_getv (e->reg, "mbcrom");
+	return r_reg_setv (e->reg, "mpc", (ut64)mpc);
+}
+
+int gb_cret (emu *e)
+{
+	ut8 c = r_reg_get_value (e->reg, e->anop->dst->reg);
+	if (e->anop->cond == R_ANAL_COND_EQ) {
+		if (c==e->anop->src[0]->imm)
+			return gb_ret (e);
+		return R_TRUE;
+	}
+	if (c!=e->anop->src[0]->imm)
+		return gb_ret (e);
+	return R_TRUE;
+}
+
+int gb_store (emu *e)
+{
+	ut64 addr;
+	ut16 buf;
+	if (e->anop->src[0]->reg)
+		buf = r_reg_get_value (e->reg, e->anop->src[0]->reg);
+	else	buf = e->anop->src[0]->imm;
+	if (e->anop->dst->reg)
+		addr = r_reg_get_value (e->reg, e->anop->dst->reg);
+	else {
+		addr = e->anop->dst->base;
+		if (e->anop->dst->regdelta)
+			addr += r_reg_get_value (e->reg, e->anop->dst->regdelta);
+	}
+	buf = buf<<(8*(e->anop->dst->memref & 1));				//is this working good?
+	if (e->op->buf_asm[2] == 'i') {						//ldi [hl]
+		addr = (addr+1) & 0xffff;
+		r_reg_set_value (e->reg, e->anop->dst->reg, addr);
+	}
+	if (e->op->buf_asm[2] == 'd') {
+		addr = (addr-1) & 0xffff;
+		r_reg_set_value (e->reg, e->anop->dst->reg, addr);
+	}
+	return gb_write (e, addr, (ut8*)(&buf), e->anop->dst->memref);
+}
+
+int gb_load (emu *e)
+{
+	ut64 addr;
+	ut8 buf;
+	if (e->anop->src[0]->reg)
+		addr = r_reg_get_value (e->reg, e->anop->src[0]->reg);
+	else {
+		addr = e->anop->src[0]->base;
+		if (e->anop->src[0]->regdelta)
+			addr += r_reg_get_value (e->reg, e->anop->src[0]->regdelta);
+	}
+	gb_read (e, addr, &buf, 1);
+	return r_reg_set_value (e->reg, e->anop->dst->reg, buf);
 }
